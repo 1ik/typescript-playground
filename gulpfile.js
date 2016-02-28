af15f5750ff169dd87610d09bcc9af3c6203e7e4
anik <anik.hasan@blackswan.com>
2016-02-28 10:44:42 +0600

    TS Helloworld, gulp project setup for transpiling TS into JS

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+ 
+var fileName = "helloworld";
+
+gulp.task('default', function () {
+	return gulp.src('src/'+fileName+'.ts')
+		.pipe(ts({
+			noImplicitAny: true,
+			out: fileName + '.js'
+		}))
+		.pipe(gulp.dest('src'));
+});
